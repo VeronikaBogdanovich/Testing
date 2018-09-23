e3ccdad47c80892140f3088f3baf80bf78f6d64d
VeronikaBogdanovich <veronicka.angel2012@yandex.by>
2018-09-23 10:37:42 +0300

    second lab - 3 commit

--- git/src/Main.java
+++ git/src/Main.java
@@ -13,20 +13,20 @@ public class Main {
         student1.addMarks(marks2);
         System.out.println("Среднее второго студента: " + student1.getAVG());
 
-        Student student3 = new Student("emma", "forse");
-        student3.addMarks(marksBest);
-        System.out.println("Среднее третьего студента: " + student3.getAVG());
+        Student student2 = new Student("emma", "forse");
+        student2.addMarks(marksBest);
+        System.out.println("Среднее третьего студента: " + student2.getAVG());
 
 
         Group group = new Group();
 
         group.addStudent(student0);
         group.addStudent(student1);
-        group.addStudent(student3);
+        group.addStudent(student2);
 
         System.out.println(student0.toString());
         System.out.println(student1.toString());
-        System.out.println(student3.toString());
+        System.out.println(student2.toString());
 
         System.out.println("Средняя по группе: " + group.getAVG());
     }
