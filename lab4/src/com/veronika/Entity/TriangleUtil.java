5f5163715c3ec57f9fb4bef237f7d83e2a5dcc11
VeronikaBogdanovich <veronicka.angel2012@yandex.by>
2018-10-19 11:29:28 +0300

    add changes

--- lab4/src/com/veronika/Entity/TriangleUtil.java
+++ lab4/src/com/veronika/Entity/TriangleUtil.java
@@ -1,13 +1,10 @@
 package com.veronika.entity;
 
 public class TriangleUtil {
-    public TriangleUtil() {
-
-    }
 
     public boolean canCreateTriangle(int a, int b, int c) {
         if (a <= 0 || b <= 0 || c <= 0) {
-            throw new IllegalArgumentException("Sides are not valid");
+            return false;
         }
 
         return a + b > c && b + c > a && a + c > b;
--- lab4/src/testing/TriangleUtilTest.java
+++ lab4/src/testing/TriangleUtilTest.java
@@ -12,71 +12,59 @@ public class TriangleUtilTest {
 
     @Before
     public void init() {
-        this.triangleUtil = new TriangleUtil();
+        triangleUtil = new TriangleUtil();
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void assertThatACanNotBeEqualToZero() {
-        this.triangleUtil.canCreateTriangle(0, 1, 2);
+    @DataProvider
+    public static Object[][] triangleZeroSides() {
+        return new Object[][]{
+                {0, 1, 2},
+                {1, 0, 2},
+                {1, 2, 0}
+        };
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void assertThatACanNotBeLessThanZero() {
-        this.triangleUtil.canCreateTriangle(-1, 1, 2);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void assertThatBCanNotBeEqualToZero() {
-        this.triangleUtil.canCreateTriangle(1, 0, 2);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void assertThatBCanNotBeLessThanZero() {
-        this.triangleUtil.canCreateTriangle(1, -1, 2);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void assertThatCCanNotBeEqualToZero() {
-        this.triangleUtil.canCreateTriangle(2, 1, 0);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void assertThatCCanNotBeLessThanZero() {
-        this.triangleUtil.canCreateTriangle(1, 1, -2);
+    @DataProvider
+    public static Object[][] triangleNegativeSides() {
+        return new Object[][]{
+                {-1, 1, 2},
+                {1, -1, 2},
+                {1, 2, -1}
+        };
     }
 
     @Test
     public void assertTrueWhenSidesAreValid() {
-        Assert.assertTrue(this.triangleUtil.canCreateTriangle(3, 4, 6));
+        Assert.assertTrue(triangleUtil.canCreateTriangle(3, 4, 6));
     }
 
     @Test
     public void assertFalseWhenSidesAreNotValid() {
-        Assert.assertFalse(this.triangleUtil.canCreateTriangle(4, 3, 9));
+        Assert.assertFalse(triangleUtil.canCreateTriangle(4, 3, 9));
     }
 
     @Test
     public void assertFalseWhenAPlusBEqualsC() {
-        Assert.assertFalse(this.triangleUtil.canCreateTriangle(3, 4, 7));
+        Assert.assertFalse(triangleUtil.canCreateTriangle(3, 4, 7));
     }
 
     @Test
     public void assertTrueWhenSidesAreEqualAndGreaterThanZero() {
-        Assert.assertTrue(this.triangleUtil.canCreateTriangle(9, 9, 9));
+        Assert.assertTrue(triangleUtil.canCreateTriangle(9, 9, 9));
     }
 
     @Test
     public void assertTrueWhenTriangleRight() {
-        Assert.assertTrue(this.triangleUtil.canCreateTriangle(3, 4, 5));
+        Assert.assertTrue(triangleUtil.canCreateTriangle(3, 4, 5));
     }
 
     @Test
     public void assertTrueWhenAEqulsBAndAPlusBMoreThanC() {
-        Assert.assertTrue(this.triangleUtil.canCreateTriangle(3, 3, 5));
+        Assert.assertTrue(triangleUtil.canCreateTriangle(3, 3, 5));
     }
 
     @Test
     public void assertFalseWhenAEqulsBAndAPlusBLessThanC() {
-        Assert.assertFalse(this.triangleUtil.canCreateTriangle(2, 2, 5));
+        Assert.assertFalse(triangleUtil.canCreateTriangle(2, 2, 5));
     }
 }
