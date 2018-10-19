d7cd63ee03c6ee6abbea98af841ff2980869ed4c
VeronikaBogdanovich <veronicka.angel2012@yandex.by>
2018-10-19 22:07:51 +0300

    Update lab4/src/testing/TriangleUtilTest.java

--- lab4/src/testing/TriangleUtilTest.java
+++ lab4/src/testing/TriangleUtilTest.java
@@ -1,13 +1,52 @@
 package testing;
 
 import com.veronika.entity.TriangleUtil;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(DataProviderRunner.class)
 public class TriangleUtilTest {
 
-    private TriangleUtil triangleUtil;
+    @DataProvider
+    public static Object[][] triangleZeroSizes() {
+        return new Object[][]{
+                {0, 4, 5},
+                {3, 0, 5},
+                {3, 4, 0}
+        };
+    }
+
+    @DataProvider
+    public static Object[][] triangleNegativeSizes() {
+        return new Object[][]{
+                {-3, 4, 5},
+                {3, -4, 5},
+                {3, 4, -5}
+        };
+    }
+
+    @DataProvider
+    public static Object[][] sidesAreNotValid() {
+        return new Object[][]{
+                {13, 4, 5},
+                {3, 14, 5},
+                {3, 4, 15}
+        };
+    }
+
+    @DataProvider
+    public static Object[][] sidesAreValid() {
+        return new Object[][]{
+                {3, 4, 6},
+                {3, 6, 5},
+                {6, 4, 5}
+        };
+    }
 
 
     @Before
@@ -16,43 +55,27 @@ public class TriangleUtilTest {
     }
 
     @Test
-    public void TriangleUtil_canCreateTriangle_AssertACanNotBeEqualToZero() {
-        Assert.assertFalse(triangleUtil.canCreateTriangle(0, 1, 2));
+    @UseDataProvider("triangleZeroSizes")
+    public void assertFalseWhenOneSideIsZero(double a, double b, double c) {
+        Assert.assertFalse(triangleUtil.canCreateTriangle(a, b, c));
     }
 
     @Test
-    public void TriangleUtil_canCreateTriangle_AssertACanNotBeLessThanZero() {
-        Assert.assertFalse(triangleUtil.canCreateTriangle(-1, 1, 2));
+    @UseDataProvider("triangleNegativeSizes")
+    public void assertFalseWhenOneSideIsNegative(double a, double b, double c) {
+        Assert.assertFalse(triangleUtil.canCreateTriangle(a, b, c));
     }
 
     @Test
-    public void TriangleUtil_canCreateTriangle_AssertBCanNotBeEqualToZero() {
-        Assert.assertFalse(triangleUtil.canCreateTriangle(1, 0, 2));
+    @UseDataProvider("sidesAreNotValid")
+    public void assertTrueWhenSidesAreValid(double a, double b, double c) {
+        Assert.assertTrue(triangleUtil.canCreateTriangle(a, b, c));
     }
 
     @Test
-    public void TriangleUtil_canCreateTriangle_AssertBCanNotBeLessThanZero() {
-        Assert.assertFalse(triangleUtil.canCreateTriangle(1, -1, 2));
-    }
-
-    @Test
-    public void TriangleUtil_canCreateTriangle_AssertCCanNotBeEqualToZero() {
-        Assert.assertFalse(triangleUtil.canCreateTriangle(2, 1, 0));
-    }
-
-    @Test
-    public void TriangleUtil_canCreateTriangle_AssertCCanNotBeLessThanZero() {
-        Assert.assertFalse(triangleUtil.canCreateTriangle(1, 1, -2));
-    }
-
-    @Test
-    public void assertTrueWhenSidesAreValid() {
-        Assert.assertTrue(triangleUtil.canCreateTriangle(3, 4, 6));
-    }
-
-    @Test
-    public void assertFalseWhenSidesAreNotValid() {
-        Assert.assertFalse(triangleUtil.canCreateTriangle(4, 3, 9));
+    @UseDataProvider("sidesAreNotValid")
+    public void assertFalseWhenSidesAreNotValid(double a, double b, double c) {
+        Assert.assertFalse(triangleUtil.canCreateTriangle(a, b, c));
     }
 
     @Test
